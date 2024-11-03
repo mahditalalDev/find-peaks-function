8931052f4641067a983b124b944844f72dc9b828
Mahdi Talal <mahditalal.789@gmail.com>
2024-11-03 11:16:44 +0200

    add the bigO notation

--- find_peaks.py
+++ find_peaks.py
@@ -1,14 +1,14 @@
-# find index of  index of the max
+# find index of  index and the  max of the list
 
-def findPeaks(list):
-    temp_peak_index_list = [list[0],0]               # give the list initial values      
-    for i in range (1,len(list)): 
-        if list[i] > temp_peak_index_list[0]:
-            temp_peak_index_list[0] = list[i]      # store peak value into temp list
-            temp_peak_index_list[1] = i            # store index of temp value into temp list
-    return(temp_peak_index_list)                   # the function will retrun list of two values peak and its index
+def findPeaks(list):                            #O(1)                            
+    temp_peak_index_list = [list[0],0]          #O(1)      # give the list initial values      
+    for i in range (1,len(list)):               #O(1en(list)) in my code the len of list Constant
+        if list[i] > temp_peak_index_list[0]:   #O(1)
+            temp_peak_index_list[0] = list[i]   #O(1)      # store peak value into temp list
+            temp_peak_index_list[1] = i         #O(1)      # store index of temp value into temp list
+    return(temp_peak_index_list)                #O(1)      # the function will return list of two values peak and its index
 
 
 
-peak_index=findPeaks([1,4,5,12,1,4,2])    
-print(f"the peak value of the list {peak_index[0]} and its index ={peak_index[1]} ")
+peak_index=findPeaks([1,4,5,12,1,4,2])          #O(1)
+print(f"the peak value of the list {peak_index[0]} and its index ={peak_index[1]} ") #O(1)
