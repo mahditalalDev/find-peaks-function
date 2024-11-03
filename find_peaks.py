2ae147e375d1fc505a5935ad7b890fe15aa282d3
Mahdi Talal <mahditalal.789@gmail.com>
2024-11-03 11:10:38 +0200

    improve the function to returns the peak value and its index

--- find_peaks.py
+++ find_peaks.py
@@ -1,11 +1,14 @@
-# find index of max
+# find index of  index of the max
 
 def findPeaks(list):
-    peak = list[0]
-    temp_index = 0
+    temp_peak_index_list = [list[0],0]               # give the list initial values      
     for i in range (1,len(list)): 
-        if list[i] > peak :
-            peak = list[i]
-            temp_index = i
-    return(temp_index)
-print(findPeaks([1,2,3,5,1]))
+        if list[i] > temp_peak_index_list[0]:
+            temp_peak_index_list[0] = list[i]      # store peak value into temp list
+            temp_peak_index_list[1] = i            # store index of temp value into temp list
+    return(temp_peak_index_list)                   # the function will retrun list of two values peak and its index
+
+
+
+peak_index=findPeaks([1,4,5,12,1,4,2])    
+print(f"the peak value of the list {peak_index[0]} and its index ={peak_index[1]} ")
