5caf7bdcf5df433ebf55af33ad353d24c9eecc39
Mahdi Talal <mahditalal.789@gmail.com>
2024-11-04 13:51:06 +0200

    update my code ,after understanding the ex

--- find_peaks.py
+++ find_peaks.py
@@ -1,14 +1,15 @@
-# find index of  index and the  max of the list
+# find index of  index and the  peaks of the list
 
-def findPeaks(list):                            #O(1)                            
-    temp_peak_index_list = [list[0],0]          #O(1)      # give the list initial values      
-    for i in range (1,len(list)):               #O(1en(list)) in my code the len of list Constant
-        if list[i] > temp_peak_index_list[0]:   #O(1)
-            temp_peak_index_list[0] = list[i]   #O(1)      # store peak value into temp list
-            temp_peak_index_list[1] = i         #O(1)      # store index of temp value into temp list
-    return(temp_peak_index_list)                #O(1)      # the function will return list of two values peak and its index
-
-
-
-peak_index=findPeaks([1,4,5,12,1,4,2])          #O(1)
-print(f"the peak value of the list {peak_index[0]} and its index ={peak_index[1]} ") #O(1)
+def findPeaks(list):
+    temp_peaks_list=[]
+    temp_index_peak_list=[]
+    for i in range(len(list)-1):
+        if i==0:
+            continue
+        if list[i] > list[i-1] and list[i] > list[i+1]:
+            temp_peaks_list.append(list[i])
+            temp_index_peak_list.append(i)
+        
+    print(temp_peaks_list)
+    print(temp_index_peak_list)
+findPeaks([0,4,2,10,4,5])
